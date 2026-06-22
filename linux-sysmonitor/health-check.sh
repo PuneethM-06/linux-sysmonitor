80eb7655977e52ea38d8ff40dfc07edaf31f8b4e
PuneethM-06 <puneethm2736@gmail.com>
2026-06-22 12:40:02 +0000

    network check

--- linux-sysmonitor/health-check.sh
+++ linux-sysmonitor/health-check.sh
@@ -91,7 +91,7 @@ check_top_processes() {
 check_top_processes
 
 check_network () {
-    NETWORK_CHECK=$(ping -c 4 8.8.8.8) 
+    NETWORK_CHECK=$(ping -c 4 8.8.8.8)
     PING_STATUS=$?
     PING_LATENCY=$(echo "$NETWORK_CHECK"| grep "rtt" | awk '{print $4}' | awk -F'/' '{print $2}')
     if [ $PING_STATUS -eq 0 ] ; then
